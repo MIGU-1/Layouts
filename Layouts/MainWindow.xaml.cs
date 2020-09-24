60f3115861296133a0c0e94eb3cfb18a0f74f357
MIGU <m.gutenbrunner@itag.at>
2020-09-24 18:04:40 +0200

    Stockpanel

--- Layouts/MainWindow.xaml.cs
+++ Layouts/MainWindow.xaml.cs
@@ -23,8 +23,41 @@ namespace Layouts
         public MainWindow()
         {
             InitializeComponent();
-            StackPanel newWindow = new StackPanel();
-            newWindow.Show();
+            stackpanelBtn.Click += btnClick;
+            dockBtn.Click += btnClick;
+            gridBtn.Click += btnClick;
+            gridsplitterBtn.Click += btnClick;
+        }
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn == null)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                switch(btn.Name)
+                {
+                    case "stackpanelBtn":
+                        StackPanelWindow stackpanel = new StackPanelWindow();
+                        stackpanel.Show();
+                        break;
+                    case "dockBtn":
+                        DockWindow dockwindow = new DockWindow();
+                        dockwindow.Show();
+                        break;
+                    case "gridBtn":
+                        GridWindow gridWindow = new GridWindow();
+                        gridWindow.Show();
+                        break;
+                    case "gridsplitterBtn":
+                        GridSplitterWindow gridsplitterWindow = new GridSplitterWindow();
+                        gridsplitterWindow.Show();
+                        break;
+                }
+            }
         }
     }
 }
